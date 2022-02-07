91fe7e7794d23755841b694c77169dabc917e582
akghule <akshayghule1110@gmail.com>
2022-02-07 08:31:03 +0530

    sfdc

---
+++ Assignment6B.java
@@ -0,0 +1,29 @@
+package com.capgemini;
+
+import java.util.Scanner;
+
+public class Assignment6B {
+
+	public static void main(String[] args) {
+		
+		//Take input from the user
+        //Create an instance of the Scanner Class
+		
+        Scanner sc=new Scanner(System.in);
+        
+        //Declare and Initialize the variable
+        
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
+        int i=1,fact=1;
+        while(i<=num)
+        {
+            fact=fact*i;
+            i++;
+        }
+        System.out.println("Factorial of the number: "+fact); 
+	   }
+
+	}
+
+
